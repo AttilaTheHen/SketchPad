5cc1e794a8f50a05d861083fc2c12019561173c0
injoongyoon <injoongyoon@gmail.com>
2017-12-08 13:18:21 -0800

    Begin to add basic hover functionality, tweak CSS and JS some as well

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let userNum = 16;
+let userNum = parseInt(window.prompt("Please enter a number:"));
 let i = 0;
+
 while (i < userNum * userNum) {
   const div = document.createElement('div');
   div.classList.add('box');
-  div.style.width = Math.floor((750 / userNum - 2)).toString() + 'px';
-  div.style.height = Math.floor((750 / userNum - 2)).toString() + 'px';
+  div.setAttribute('id', 'boxes')
+  div.style.width = (750 / userNum - 2).toString() + 'px';
+  div.style.height = (750 / userNum - 2).toString() + 'px';
   container.appendChild(div);
   i++;
 }
+
+const test = document.getElementById('boxes')
+test.addEventListener("mouseover", function(event) {
+  event.target.style.background = 'powderblue';
+});
