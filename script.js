6950e99b89fbed0ab46cb2445d0b405ca82bb6be
injoongyoon <injoongyoon@gmail.com>
2017-12-11 11:56:52 -0800

    Adjust initial page load to automatically have 16x16 grid, make button ask for user input

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let userNum = parseInt(window.prompt("Please enter a number:"));
-let i = 0;
+function setGrid(num) {
+  let i = 0;
 
-while (i < userNum * userNum) {
-  const div = document.createElement('div');
-  div.classList.add('box');
-  div.setAttribute('id', 'boxes' + i + '')
-  div.style.width = (750 / userNum - 2).toString() + 'px';
-  div.style.height = (750 / userNum - 2).toString() + 'px';
-  container.appendChild(div);
-  i++;
+  while (i < num * num) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.setAttribute('id', 'boxes' + i + '')
+    div.style.width = (750 / num - 2).toString() + 'px';
+    div.style.height = (750 / num - 2).toString() + 'px';
+    container.appendChild(div);
+    i++;
+  }
+
+  let boxArray = document.getElementsByClassName('box');
+  for (let j = 0; j < boxArray.length; j++) {
+    const test = boxArray[j];
+    test.addEventListener('mouseover', function(event) {
+      event.target.style.background = 'powderblue';
+    });
+  }
 }
 
-let boxArray = document.getElementsByClassName('box');
-
-for (let j = 0; j < boxArray.length; j++) {
-  const test = boxArray[j];
-  test.addEventListener('mouseover', function(event) {
-    event.target.style.background = 'powderblue';
-  });
+function getNum() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let userNum = parseInt(prompt("Please enter a number: "));
+  setGrid(userNum);
 }
+
+setGrid(16);
 
 let btn = document.querySelector('button');
-btn.addEventListener('click', function(event) {
-  window.location.reload(event);
-});
+btn.addEventListener('click', getNum);
